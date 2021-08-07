5cab7859a085c488bd5df8d20745f92cc92fcc00
Tenrai <tenraiaisaka@gmail.com>
2021-08-07 22:34:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import random
+
 qua_size = 3  # Количество переменных
-#task_original = 'a * b + - c'  # Задание
-#task_original = '- a + a * c + b'  # Задание
-#task_original = 'a + b * c'  # Задание пройдено
-task_original = 'a * c + b'  # Задание
+max_operations = 4
+
+task = []
 
 
-def transformation(task: str, separator=' ') -> str:
-    """ Преобразует изначальную строку в строку, которую понимает интерпритатор"""
+""" def transformation(task: str, separator=' ') -> str:
+     Преобразует изначальную строку в строку, которую понимает интерпритатор
 
     task_final = task.split(separator)  # Разделение строки по пробелу в список
 
@@ -21,21 +22,21 @@ def transformation(task: str, separator=' ') -> str:
 
     task_final = ' '.join(task_final)  # Конечный вариант строки, которая обрабатывается интерпритатором
 
-    return task_final
+    return task_final """
 
 
 def matrix_calculation(size: int) -> tuple:
-    """Рассчет и заполнение маьтрицы истинности операндов"""
+    """Рассчет и заполнение матрицы истинности операндов"""
 
     '''Рассчет размера матрицы'''
-    matrix_heigh = 2 ** size  # Количество строк
+    matrix_height = 2 ** size  # Количество строк
     matrix_width = size + 1  # Количество колонок
 
     '''Создание матрицы'''
-    matrix = [['?' for i in range(matrix_width)] for j in range(matrix_heigh)]
+    matrix = [['?' for i in range(matrix_width)] for j in range(matrix_height)]
 
-    ''' Присвоение таблице истинности 0 и 1'''
-    for i in range(matrix_heigh):
+    '''Присвоение таблице истинности 0 и 1'''
+    for i in range(matrix_height):
         for j in range(3):
             if j == 0 and i <= 3:
                 matrix[i][j] = False
@@ -47,29 +48,31 @@ def matrix_calculation(size: int) -> tuple:
             elif j == 2 and i % 2 == 1:
                 matrix[i][j] = True
 
-    for i in range(0, matrix_heigh, 4):
+    for i in range(0, matrix_height, 4):
         matrix[i][1] = False
         matrix[i + 1][1] = False
 
-    for i in range(2, matrix_heigh, 4):
+    for i in range(2, matrix_height, 4):
         matrix[i][1] = True
         matrix[i + 1][1] = True
 
 
-    return matrix, matrix_heigh, matrix_width
+    return matrix, matrix_height, matrix_width
 
 
 
-def solution():
-    matrix, heigh, width = matrix_calculation(qua_size)
-    task = transformation(task_original)
+def solution() -> list:
+    """ Решение матрицы """
+    matrix, height, width = matrix_calculation(qua_size)
+    task = generation(max_operations)
 
-    for row in range(heigh):
+    for row in range(height):
         a = matrix[row][0]
         b = matrix[row][1]
         c = matrix[row][2]
         matrix[row][width-1] = eval(task)
     print(task)
+
     return matrix
 
 
@@ -85,10 +88,69 @@ def show_matrix():
 
 
 
+def generation(max_operation: int) -> list:
+    operation = 0
+    exam_a = 0
+    exam_b = 0
+    exam_c = 0
+
+    task_original11 = []
+    oper_and_var = ['a', 'b', 'c', 'not']
+    vari = ['a', 'b', 'c']
+    oper = ['or', 'and']
+    not_and_var = ['not', 'a', 'b', 'c']
+    while operation < max_operation:
+        if task_original11 == []:
+            task_original11.append(random.choice(oper_and_var))
+        else:
+            if task_original11[-1] == 'not':
+                task_original11.append(random.choice(vari))
+            elif task_original11[-1] in 'abc':
+                task_original11.append(random.choice(oper))
+                operation += 1
+            else:
+                task_original11.append(random.choice(not_and_var))
+
+        if task_original11[-1] == 'a':
+            exam_a += 1
+        elif task_original11[-1] == 'b':
+            exam_b += 1
+        elif task_original11[-1] == 'c':
+            exam_c += 1
+
+
+
+    if exam_a == 0:
+        task_original11.append('a')
+    elif exam_b == 0:
+        task_original11.append('b')
+    elif exam_b == 0:
+        task_original11.append('c')
+    else:
+        task_original11.append(random.choice(vari))
+
+    task_original11 = ' '.join(task_original11)  # Конечный вариант строки, которая обрабатывается интерпритатором
+
+    
+    return task_original11
+
+
+
+def examination():
+    """ Создает матрицу, с которой будет работать пользователь """
+
+    exam_matrix, height, width = matrix_calculation(qua_size)
+    print(exam_matrix)
+
+
+
 
 def main():
-    """Основная функция программы"""
+    """ Основная функция программы """
     show_matrix()
+
+
+
 
 main()
 
