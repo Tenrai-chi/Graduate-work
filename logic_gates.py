075ed4bdf5bba67e9de1b3827aa178bcf0eef539
Tenrai <tenraiaisaka@gmail.com>
2021-08-20 23:47:49 +0300

    Update logic_gates.py

--- logic_gates.py
+++ logic_gates.py
@@ -1,3 +1,11 @@
+import random
+
+gates_for_1 = ['not', 'buf']
+gates_for_2 = ['and', 'or', 'xor', 'nand', 'nor', 'xnor']
+gates_for_3 = ['nor3']
+true_false = [True, False]
+
+
 def log_NOT(a: bool) -> bool:
     """ Вентилль НЕ
         Возвращает инвертированное значение входящей переменной """
@@ -10,6 +18,7 @@ def log_BUF(a: bool) -> bool:
     """ Вентиль БУФФЕР
         Возвращает входящее значение """
     y = a
+
     return y
 
 
@@ -43,7 +52,7 @@ def log_XOR(a, b: bool) -> bool:
 def log_NAND(a, b: bool) -> bool:
     """ Вентиль И-НЕ
         Возвращает истиннцу, если хотя бы один из входящих значений ложно
-        Инвертирует значения вентиля И"""
+        Инвертирует значения вентиля И """
     if a == False or b == False:
         y = True
     else:
@@ -83,3 +92,213 @@ def log_NOR3(a, b, c: bool) -> bool:
     else:
         y = False
     return y
+
+
+def pattern_1():
+    """ Шаблон 1 """
+
+    a = random.choice(true_false)
+    b = random.choice(true_false)
+    c = random.choice(true_false)
+
+    elements = []
+    for i in range(5):
+        elements.append('?')
+
+    elements[0] = random.choice(gates_for_1)
+    elements[1] = random.choice(gates_for_1)
+    elements[2] = random.choice(gates_for_2)
+    elements[3] = random.choice(gates_for_2)
+    elements[4] = random.choice(gates_for_2)
+    print(elements, a, b, c)
+
+    ans_elements = []
+    for x in range(2):
+        temp = solution(elements[x], a, b, c)
+        ans_elements.append(temp)
+
+    temp = solution(elements[2], ans_elements[0], ans_elements[1])
+    ans_elements.append(temp)
+
+    temp = solution(elements[3], b, c)
+    ans_elements.append(temp)
+
+    temp = solution(elements[4], ans_elements[2], ans_elements[3])
+    ans_elements.append(temp)
+
+    print(ans_elements)
+
+
+def pattern_2():
+    """ Шаблон 2 """
+
+    a = random.choice(true_false)
+    b = random.choice(true_false)
+    c = random.choice(true_false)
+
+    elements = []
+    for i in range(4):
+        elements.append('?')
+
+    elements[0] = random.choice(gates_for_2)
+    elements[1] = random.choice(gates_for_2)
+    elements[2] = random.choice(gates_for_1)
+    elements[3] = random.choice(gates_for_2)
+    print(elements, a, b, c)
+
+    ans_elements = []
+
+    temp = solution(elements[0], a, b)
+    ans_elements.append(temp)
+
+    temp = solution(elements[1], b, c)
+    ans_elements.append(temp)
+
+    temp = solution(elements[2], ans_elements[0])
+    ans_elements.append(temp)
+
+    temp = solution(elements[3], ans_elements[2], ans_elements[1])
+    ans_elements.append(temp)
+
+    print(ans_elements)
+
+
+def pattern_3():
+    """ Шаблон 3 """
+    a = random.choice(true_false)
+    b = random.choice(true_false)
+    c = random.choice(true_false)
+
+    elements = []
+    for i in range(5):
+        elements.append('?')
+
+    elements[0] = random.choice(gates_for_2)
+    elements[1] = random.choice(gates_for_2)
+    elements[2] = random.choice(gates_for_1)
+    elements[3] = random.choice(gates_for_1)
+    elements[4] = random.choice(gates_for_2)
+    print(elements, a, b, c)
+
+    ans_elements = []
+
+    temp = solution(elements[0], a, b)
+    ans_elements.append(temp)
+
+    temp = solution(elements[1], b, c)
+    ans_elements.append(temp)
+
+    temp = solution(elements[2], ans_elements[0])
+    ans_elements.append(temp)
+
+    temp = solution(elements[3], ans_elements[1])
+    ans_elements.append(temp)
+
+    temp = solution(elements[4], ans_elements[2], ans_elements[3])
+    ans_elements.append(temp)
+
+    print(ans_elements)
+
+
+def pattern_4():
+    """ Шаблон 4 """
+    a = random.choice(true_false)
+    b = random.choice(true_false)
+    c = random.choice(true_false)
+
+    elements = []
+    for i in range(5):
+        elements.append('?')
+
+    elements[0] = random.choice(gates_for_2)
+    elements[1] = random.choice(gates_for_2)
+    elements[2] = random.choice(gates_for_2)
+    elements[3] = random.choice(gates_for_1)
+    elements[4] = random.choice(gates_for_2)
+    print(elements, a, b, c)
+
+    ans_elements = []
+
+    temp = solution(elements[0], a, b)
+    ans_elements.append(temp)
+
+    temp = solution(elements[1], b, c)
+    ans_elements.append(temp)
+
+    temp = solution(elements[2], ans_elements[0], ans_elements[1])
+    ans_elements.append(temp)
+
+    temp = solution(elements[3], b)
+    ans_elements.append(temp)
+
+    temp = solution(elements[4], ans_elements[2], ans_elements[3])
+    ans_elements.append(temp)
+
+    print(ans_elements)
+
+
+def pattern_5():
+    """ Шаблон 5 """
+    a = random.choice(true_false)
+    b = random.choice(true_false)
+    c = random.choice(true_false)
+
+    elements = []
+    for i in range(5):
+        elements.append('?')
+
+    elements[0] = random.choice(gates_for_2)
+    elements[1] = random.choice(gates_for_2)
+    elements[2] = random.choice(gates_for_1)
+    elements[3] = random.choice(gates_for_2)
+    elements[4] = random.choice(gates_for_1)
+    print(elements, a, b, c)
+
+    ans_elements = []
+
+    temp = solution(elements[0], a, b)
+    ans_elements.append(temp)
+
+    temp = solution(elements[1], b, c)
+    ans_elements.append(temp)
+
+    temp = solution(elements[2], ans_elements[0])
+    ans_elements.append(temp)
+
+    temp = solution(elements[3], ans_elements[2], ans_elements[1])
+    ans_elements.append(temp)
+
+    temp = solution(elements[4], ans_elements[3])
+    ans_elements.append(temp)
+
+    print(ans_elements)
+
+
+def solution(operation, enter_a, enter_b=True, enter_c=True) -> bool:
+    """ решение выражения с заданным действием и параметрами """
+    #Выражения с 1 входящим значением
+    if operation == 'not':
+        return log_NOT(enter_a)
+    elif operation == 'buf':
+        return log_BUF(enter_a)
+
+    #Выражения с 2 входящими значениеми
+    elif operation == 'and':
+        return log_AND(enter_a, enter_b)
+    elif operation == 'or':
+        return log_OR(enter_a, enter_b)
+    elif operation == 'xor':
+        return log_XOR(enter_a, enter_b)
+    elif operation == 'nand':
+        return log_NAND(enter_a, enter_b)
+    elif operation == 'nor':
+        return log_NOR(enter_a, enter_b)
+    elif operation == 'xnor':
+        return log_XNOR(enter_a, enter_b)
+
+    #Выражения с 3 входящими значениеми  '''
+    else:
+        return log_NOR3(enter_a, enter_b, enter_c)
+
+
+pattern_5()
